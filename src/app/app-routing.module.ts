23123cd56f7c14039737a00dc3fbf4dc1334afa4
Arun Vijayarengan <arunprabu.v@hotmail.com>
2021-12-23 14:02:22 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/components/home/home.component';
+import { ConceptsComponent } from './concepts/components/concepts/concepts.component';
+import { ContactsComponent } from './contacts/components/contacts/contacts.component';
+
+// configuring routes
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'concepts', component: ConceptsComponent },
+  { path: 'contacts', component: ContactsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], // registering the configured routes
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/concepts/components/concepts/concepts.component.ts
+++ src/app/concepts/components/concepts/concepts.component.ts
@@ -8,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConceptsComponent implements OnInit {
 
-  constructor() { }
+  // interpolation related
+  appName = "Contact Manager";
+  teamSize = 7;
+
+  // prop binding related
+  companyName = "VFISLK";
+  websiteURL = 'https://www.vfislk.com/';
+
+  // two way binding related
+  courseName = "Angular";
+
+  // directives related
+  isLoggedIn = true;
+  skillList: string[] = ['html', 'css', 'ts', 'ng'];
+
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  // event binding related
+  handleClickMe(event: Event): void {
+    alert("Clicked");
+    console.log(event);
+    // TODO: Change the button label to 'Clicked'
+    // TODO: Learn about disabling the button
   }
 
 }
